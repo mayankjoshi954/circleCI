602f4b3e065acb7210466ceb84e59197e917e8c5
mayankjoshi954 <80168099+mayankjoshi954@users.noreply.github.com>
2022-11-15 15:59:30 +0530

    Add files via upload

---
+++ cypress.config.js
@@ -0,0 +1,10 @@
+const { defineConfig } = require('cypress')
+
+module.exports = defineConfig({
+  projectId: 't7h7ye',
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  }
+})
